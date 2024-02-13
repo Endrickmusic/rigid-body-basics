7b8211861a60d136aac1f0985b86adb1dc6b476e
Christian <mail@christianhohenbild.de>
2024-02-13 15:47:48 +0100

    remove leva

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useControls, Leva } from 'leva'
 
 import './App.css'
 
@@ -9,19 +8,14 @@ import Cube from './Cube.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
-  const config = useControls('Controls', {
-    backgroundColor : '#ffffff'
-  })
-
   return (
   <>
-    <Leva collapsed/>
     <Canvas
     camera={{ 
       position: [4.5, 2.5,5.5],
       fov: 40 }}  
     >
-      <color attach="background" args={[config.backgroundColor]} />
+      <color attach="background" args={['#b1a3c2']} />
       <Cube />
     </Canvas>
   </>
